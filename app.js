bd6c8a66f513d13d5342fa5ff7b04961696ecab6
Gargenziano03 <gargenziano03@gmail.com>
2024-11-05 17:22:44 +0100

    add create a show method in the controller file

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
 const express = require('express')
 const app = express()
 const posts = require('./db/posts.js')
+const postController = require('./controllers/postController.js')
 
 app.listen(3000, () => {
     console.log('server started on port 3000');
 });
 
-app.get('/posts', (req, res) => {
-    res.json({
-        data: posts,
-        count: posts.length
-    })
-});
+app.get('/posts', postController.index);
 
-app.get('/posts/:title', (req, res) => {
-    const post = posts.find((post) => post.title.toLowerCase() === req.params.title) 
-    if (!post) {
-        return res.status(404).json({ error: 'no post found with tha title'})
-    }
-    return res.status(200).json({ data: post})
-});
+app.get('/posts/:title', postController.show);
--- controllers/postController.js
+++ controllers/postController.js
@@ -4,6 +4,15 @@ const index = (req, res) => {
     res.json({ data: posts, count: posts.length})
 }
 
+const show = (req, res) => {
+    const post = posts.find((post) => post.title.toLowerCase() === req.params.title) 
+    if (!post) {
+        return res.status(404).json({ error: 'no post found with tha title'})
+    }
+    return res.status(200).json({ data: post})
+};
+
 module.exports = {
-    index
+    index,
+    show
 }
