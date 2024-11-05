361c9d275f764d7b72071a02edd8153637477d12
Gargenziano03 <gargenziano03@gmail.com>
2024-11-05 15:48:27 +0100

    add import our posts and create our first endpoint

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
+const posts = require('./db/posts.js')
 
 app.listen(3000, () => {
     console.log('server started on port 3000');
+});
+
+app.get('/posts', (req, res) => {
+    res.json({
+        data: posts,
+        count: posts.length
+    })
 });
