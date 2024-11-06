d4763a513c89f97e8d21d96386008d9b1f46c720
Gargenziano03 <gargenziano03@gmail.com>
2024-11-06 18:06:00 +0100

    add (show)

--- bonus/app.js
+++ bonus/app.js
@@ -14,3 +14,13 @@ app.listen(3000, () => {
 app.get('/games', (req, res) => {
     res.json({data: games, count: games.lengt })
 });
+
+//get a single game by its ID (show)
+app.get('/games/:id', (req, res) => {
+    //find the game by id
+    const game = games.find((game) => game.id === parseInt(req.params.id))
+    if (!game) {
+        return res.status(404).json({ error: "No game found with that id"})
+    }
+    return res.status(200).json({data: game})
+});
