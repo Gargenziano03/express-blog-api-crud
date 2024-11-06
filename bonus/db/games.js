3dfc328b9285efa4bc48b23a32228cce96b04aa9
Gargenziano03 <gargenziano03@gmail.com>
2024-11-06 17:43:35 +0100

    add bonus

---
+++ bonus/db/games.js
@@ -0,0 +1,74 @@
+const games = [
+    {
+        id: 1,
+        titolo: "The Legend of Zelda: Breath of the Wild",
+        piattaforma: "Nintendo Switch",
+        genere: "Azione/Avventura",
+        anno: 2017
+    },
+    {
+        id: 2,
+        titolo: "The Witcher 3: Wild Hunt",
+        piattaforma: "PC, PS4, Xbox One, Nintendo Switch",
+        genere: "GDR",
+        anno: 2015
+    },
+    {
+        id: 3,
+        titolo: "Minecraft",
+        piattaforma: "PC, PS4, Xbox, Switch, Mobile",
+        genere: "Sandbox",
+        anno: 2011
+    },
+    {
+        id: 4,
+        titolo: "Red Dead Redemption 2",
+        piattaforma: "PS4, Xbox One, PC",
+        genere: "Azione/Avventura",
+        anno: 2018
+    },
+    {
+        id: 5,
+        titolo: "Dark Souls III",
+        piattaforma: "PS4, Xbox One, PC",
+        genere: "Azione/RPG",
+        anno: 2016
+    },
+    {
+        id: 6,
+        titolo: "Fortnite",
+        piattaforma: "PC, PS4, Xbox One, Switch, Mobile",
+        genere: "Battle Royale",
+        anno: 2017
+    },
+    {
+        id: 7,
+        titolo: "Super Mario Odyssey",
+        piattaforma: "Nintendo Switch",
+        genere: "Piattaforma",
+        anno: 2017
+    },
+    {
+        id: 8,
+        titolo: "Hades",
+        piattaforma: "PC, Switch, PS4, Xbox One",
+        genere: "Roguelike, Azione",
+        anno: 2020
+    },
+    {
+        id: 9,
+        titolo: "Overwatch",
+        piattaforma: "PC, PS4, Xbox One, Switch",
+        genere: "Sparatutto, Multigiocatore",
+        anno: 2016
+    },
+    {
+        id: 10,
+        titolo: "Cyberpunk 2077",
+        piattaforma: "PC, PS4, Xbox One, PS5, Xbox Series X/S",
+        genere: "GDR, Azione",
+        anno: 2020
+    }
+];
+
+module.exports = games
