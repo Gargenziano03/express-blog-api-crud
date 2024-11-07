59e5accf8499c34723f02f66d9720f850c46769c
Gargenziano03 <gargenziano03@gmail.com>
2024-11-07 14:35:08 +0100

    add notfound

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const postRoutes = require('./routes/post.js')
+const notFoundMiddleware = require('./middlewares/notFound.js')
 app.use(express.json())
 
 app.listen(3000, () => {
@@ -8,3 +9,4 @@ app.listen(3000, () => {
 });
 
 app.use('/posts', postRoutes)
+app.use(notFoundMiddleware)
