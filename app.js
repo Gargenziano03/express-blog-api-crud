b86caa34d10d378ce9d4bd8d70201e9deefa5073
Gargenziano03 <gargenziano03@gmail.com>
2024-11-07 14:52:24 +0100

    add error 500

--- app.js
+++ app.js
@@ -2,11 +2,25 @@ const express = require('express')
 const app = express()
 const postRoutes = require('./routes/post.js')
 const notFoundMiddleware = require('./middlewares/notFound.js')
+const loggerMiddleware = require('./middlewares/loggerMiddleware.js')
 app.use(express.json())
 
 app.listen(3000, () => {
     console.log('server started on port 3000');
 });
 
+app.use('/posts', (req, res, next) => {
+    throw new Error('you brke everything dude!');
+})
+app.use('/posts', loggerMiddleware)
 app.use('/posts', postRoutes)
 app.use(notFoundMiddleware)
+
+app.use ((err, req, res, next) => {
+    console.log('Error:', err.message);
+    console.error(err.stack);
+    res.status(500).send({
+        message: 'Something went wrong',
+        error: err.message
+    })    
+});
