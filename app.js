2bc5aaa5a8ed40da11ce7e5d55f1fb8928b8892b
Gargenziano03 <gargenziano03@gmail.com>
2024-11-07 14:54:13 +0100

    finish

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use('/posts', postRoutes)
 app.use(notFoundMiddleware)
 
 app.use ((err, req, res, next) => {
-    console.log('Error:', err.message);
+    //console.log('Error:', err.message);
     console.error(err.stack);
     res.status(500).send({
         message: 'Something went wrong',
